bcf2877b47ba99661cd675012876babaabf9b283
Niklas Wolf <wolfniklas94@web.de>
2017-06-19 22:25:37 +0200

    added VueAsset

--- frontend/assets/VueAsset.php
+++ frontend/assets/VueAsset.php
@@ -7,17 +7,16 @@ use yii\web\AssetBundle;
 /**
  * Main frontend application asset bundle.
  */
-class AppAsset extends AssetBundle
+class VueAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
     ];
     public $js = [
+    	'vue/dist/build.js',
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        'frontend\assets\AppAsset',
     ];
 }
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,7 +1,10 @@
 <?php
 
+use frontend\assets\VueAsset;
+
 /* @var $this yii\web\View */
 
 $this->title = 'Dashboard';
+VueAsset::register($this);
 ?>
 <div id="app"></div>
