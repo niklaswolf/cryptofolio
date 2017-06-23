2d5b8e93ab073ba7082e2f1ec386b169081f2175
Niklas Wolf <wolfniklas94@web.de>
2017-06-23 21:04:42 +0200

    added vuex

--- frontend/web/vue/src/main.js
+++ frontend/web/vue/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import router from './router';
+import store from './store';
 import App from './App.vue';
 
 new Vue({
   router,
+  store,
   el: '#app',
   render: h => h(App)
 })
